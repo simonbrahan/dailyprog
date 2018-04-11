0c1ff8b58306bb2279c5560e719a38a3edd02fe5
Simon Brahan <tntpiranha@gmail.com>
2018-04-11 11:20:47 +0100

    Helper functions to get index of a character, or the character at an index, of the alphabet

--- alphabet_cypher/src/main.rs
+++ alphabet_cypher/src/main.rs
@@ -3,6 +3,14 @@ use std::env;
 fn main() {
     let keyword = env::args().nth(1).unwrap();
     let body = env::args().nth(2).unwrap();
+    println!("{}", get_idx_of_char(body.chars().next().unwrap()));
+    println!("{}", get_char_at_idx(2));
+}
 
-    println!("{}, {}", keyword, body);
+fn get_idx_of_char(character: char) -> usize {
+    return "abcdefghijklmnopqrstuvwxyz".find(character).unwrap();
+}
+
+fn get_char_at_idx(loc: usize) -> char {
+    return "abcdefghijklmnopqrstuvwxyz".chars().nth(loc).unwrap();
 }
