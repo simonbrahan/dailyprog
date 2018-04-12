ef6275644c562e8e56665674643cc00ad1487e4f
Simon Brahan <tntpiranha@gmail.com>
2018-04-12 14:56:46 +0100

    2018-03-26 part 1 completed in rust

--- alphabet_cypher/src/main.rs
+++ alphabet_cypher/src/main.rs
@@ -3,14 +3,27 @@ use std::env;
 fn main() {
     let keyword = env::args().nth(1).unwrap();
     let body = env::args().nth(2).unwrap();
-    println!("{}", get_idx_of_char(body.chars().next().unwrap()));
-    println!("{}", get_char_at_idx(2));
+    let transpose_table = get_transpose_table(keyword);
+
+    let mut output = String::new();
+    for (input_idx, input_char) in body.char_indices() {
+        let transpose_distance = transpose_table[input_idx % transpose_table.len()];
+        output.push(alpha_idx_to_char((char_to_alpha_idx(input_char) + transpose_distance) % 26));
+    }
+
+    println!("{}", output);
 }
 
-fn get_idx_of_char(character: char) -> usize {
-    return "abcdefghijklmnopqrstuvwxyz".find(character).unwrap();
+fn get_transpose_table(keyword: String) -> Vec<usize> {
+    return keyword.chars()
+                  .map(|character| char_to_alpha_idx(character))
+                  .collect();
 }
 
-fn get_char_at_idx(loc: usize) -> char {
-    return "abcdefghijklmnopqrstuvwxyz".chars().nth(loc).unwrap();
+fn char_to_alpha_idx(character: char) -> usize {
+    return "abcdefghijklmnopqrstuvwxyz".find(character).expect("Bad char for char_to_alpha_idx");
+}
+
+fn alpha_idx_to_char(loc: usize) -> char {
+    return "abcdefghijklmnopqrstuvwxyz".chars().nth(loc).expect("Bad loc for alpha_idx_to_char");
 }
