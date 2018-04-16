ebc3ff96cb21ca0453a0f59ad8789021c39a66a4
Simon Brahan <tntpiranha@gmail.com>
2018-04-16 13:36:32 +0100

    Rewrote to use array instead of struct, to allow easy mapping over ingredients.

--- 355-possible_num_of_pies/src/main.rs
+++ 355-possible_num_of_pies/src/main.rs
@@ -1,34 +1,31 @@
 use std::env;
 
-#[derive(Debug)]
-struct Ingredients {
-    pumpkin: usize,
-    apples: usize,
-    eggs: usize,
-    milk: usize,
-    sugar: usize
+type Ingredients = [usize; 5];
+
+fn ingredients_from_comma_separated(list: &str) -> Ingredients {
+    let parts: Vec<usize> = list
+        .split(",")
+        .map(
+            |ingredient_count|
+                ingredient_count
+                    .parse::<usize>()
+                    .expect("Ingredient input must be five comma separated positive integers eg 10,0,4,3,2")
+        )
+        .collect();
+
+    return [parts[0], parts[1], parts[2], parts[3], parts[4]];
 }
 
-impl Ingredients {
-    fn from_cs(list: &str) -> Ingredients {
-        let ingredient_counts: Vec<usize> = list
-            .split(",")
-            .map(
-                |ingredient_count|
-                    ingredient_count
-                        .parse::<usize>()
-                        .expect("Ingredient input must be five comma separated positive integers eg 10,0,4,3,2")
-            )
-            .collect();
+fn num_can_bake(available: &Ingredients, recipe: &Ingredients) -> usize {
+    return available.iter().zip(recipe.iter()).map(
+        |(available_count, recipe_requirement)| {
+            if recipe_requirement == &0 {
+                return usize::max_value();
+            }
 
-        return Ingredients {
-            pumpkin: ingredient_counts[0],
-            apples: ingredient_counts[1],
-            eggs: ingredient_counts[2],
-            milk: ingredient_counts[3],
-            sugar: ingredient_counts[4]
+            return (available_count / recipe_requirement) as usize;
         }
-    }
+    ).min().unwrap();
 }
 
 fn main() {
@@ -36,7 +33,10 @@ fn main() {
         .nth(1)
         .expect("Ingredient input must be five comma separated positive integers eg 10,0,4,3,2");
 
-    let starting_ingredients = Ingredients::from_cs(input);
-    let pumpkin_pie = Ingredients::from_cs("1,0,3,4,3");
-    let apple_pie = Ingredients::from_cs("0,1,4,3,2");
+    let starting_ingredients = ingredients_from_comma_separated(input);
+    let pumpkin_pie = ingredients_from_comma_separated("1,0,3,4,3");
+    let apple_pie = ingredients_from_comma_separated("0,1,4,3,2");
+
+    println!("{}", num_can_bake(&starting_ingredients, &pumpkin_pie));
+    println!("{}", num_can_bake(&starting_ingredients, &apple_pie));
 }
