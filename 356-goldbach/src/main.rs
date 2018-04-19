6ef9945f5836f4c4f76ea0db8bc6e9f22bddb5a5
Simon Brahan <tntpiranha@gmail.com>
2018-04-19 13:12:48 +0100

    2018-04-11 solution and formatting

--- 356-goldbach/src/main.rs
+++ 356-goldbach/src/main.rs
@@ -10,13 +10,46 @@ fn get_input() -> usize {
         .expect("Single positive integer expected as argument")
 }
 
-fn list_primes_less_than(input: &usize) -> Vec<usize> {
+fn list_primes_until(input: &usize) -> Vec<usize> {
     let sieve = primal::Sieve::new(input.clone());
-    return sieve.primes_from(0).take_while(|x| x < input).collect();
+    return sieve.primes_from(0).take_while(|x| x <= input).collect();
+}
+
+fn get_goldbach_solution(input: &usize) -> Option<[usize; 3]> {
+    let first_primes = list_primes_until(&input);
+
+    for first_prime in first_primes {
+        if input - first_prime < 1 {
+            continue;
+        }
+
+        let second_primes = list_primes_until(&(input - first_prime));
+        for second_prime in second_primes {
+            if input - first_prime - second_prime < 1 {
+                continue;
+            }
+
+            let third_primes = list_primes_until(&(input - first_prime - second_prime));
+            for third_prime in third_primes {
+                if &(first_prime + second_prime + third_prime) == input {
+                    return Some([first_prime, second_prime, third_prime]);
+                }
+            }
+        }
+    }
+
+    return None;
 }
 
 fn main() {
     let input = get_input();
 
-    println!("{:?}", list_primes_less_than(&input));
+    let solution = get_goldbach_solution(&input).expect("No solution found for input");
+    println!(
+        "{} = {} + {} + {}",
+        input,
+        solution[0],
+        solution[1],
+        solution[2]
+    );
 }
