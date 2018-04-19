1043305b1534ee1f650e192b41c5b297570659f3
Simon Brahan <tntpiranha@gmail.com>
2018-04-19 15:44:13 +0100

    list_primes_until now returns sane results for invalid input

--- 356-goldbach/src/main.rs
+++ 356-goldbach/src/main.rs
@@ -11,6 +11,10 @@ fn get_input() -> usize {
 }
 
 fn list_primes_until(input: usize) -> Vec<usize> {
+    if input < 2 {
+        return vec![];
+    }
+
     let sieve = primal::Sieve::new(input.clone());
     return sieve.primes_from(0).take_while(|x| x <= &input).collect();
 }
@@ -19,16 +23,8 @@ fn get_goldbach_solution(input: usize) -> Option<[usize; 3]> {
     let first_primes = list_primes_until(input);
 
     for first_prime in first_primes {
-        if input - first_prime < 1 {
-            continue;
-        }
-
         let second_primes = list_primes_until(input - first_prime);
         for second_prime in second_primes {
-            if input - first_prime - second_prime < 1 {
-                continue;
-            }
-
             let third_primes = list_primes_until(input - first_prime - second_prime);
             for third_prime in third_primes {
                 if first_prime + second_prime + third_prime == input {
